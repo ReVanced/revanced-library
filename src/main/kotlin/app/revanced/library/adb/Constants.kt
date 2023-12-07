9fda40744173669c84b0c2599ae5ac5d39591798
oSumAtrIX <johan.melkonyan1@web.de>
2023-12-07 16:39:41 +0100

    feat: Improve mount reliability by unmounting existing mounts and killing running apps

--- src/main/kotlin/app/revanced/library/adb/Constants.kt
+++ src/main/kotlin/app/revanced/library/adb/Constants.kt
@@ -6,11 +6,12 @@ internal object Constants {
     internal const val TMP_PATH = "/data/local/tmp/revanced.tmp"
     internal const val INSTALLATION_PATH = "/data/adb/revanced/"
     internal const val PATCHED_APK_PATH = "$INSTALLATION_PATH$PLACEHOLDER.apk"
-    internal const val MOUNT_PATH = "/data/adb/service.d/mount_revanced_$PLACEHOLDER.sh"
+    internal const val MOUNT_SCRIPT_PATH = "/data/adb/service.d/mount_revanced_$PLACEHOLDER.sh"
 
     internal const val DELETE = "rm -rf $PLACEHOLDER"
     internal const val CREATE_DIR = "mkdir -p"
     internal const val RESTART = "am start -S $PLACEHOLDER"
+    internal const val KILL = "am force-stop $PLACEHOLDER"
     internal const val GET_INSTALLED_PATH = "pm path $PLACEHOLDER"
 
     internal const val INSTALL_PATCHED_APK =
@@ -23,7 +24,7 @@ internal object Constants {
     internal const val UMOUNT =
         "grep $PLACEHOLDER /proc/mounts | while read -r line; do echo ${'$'}line | cut -d ' ' -f 2 | sed 's/apk.*/apk/' | xargs -r umount -l; done"
 
-    internal const val INSTALL_MOUNT = "mv $TMP_PATH $MOUNT_PATH && chmod +x $MOUNT_PATH"
+    internal const val INSTALL_MOUNT_SCRIPT = "mv $TMP_PATH $MOUNT_SCRIPT_PATH && chmod +x $MOUNT_SCRIPT_PATH"
 
     internal val MOUNT_SCRIPT =
         """
@@ -33,11 +34,17 @@ internal object Constants {
 
         until [ "$( getprop sys.boot_completed )" = 1 ]; do sleep 3; done
         until [ -d "/sdcard/Android" ]; do sleep 1; done
-        
+
+        # Unmount any existing mount as a safety measure
+        $UMOUNT
+
         base_path="$PATCHED_APK_PATH"
         stock_path=$( pm path $PLACEHOLDER | grep base | sed 's/package://g' )
 
-        chcon u:object_r:apk_data_file:s0  ${'$'}base_path
+        chcon u:object_r:apk_data_file:s0 ${'$'}base_path
         mount -o bind ${'$'}MIRROR${'$'}base_path ${'$'}stock_path
+
+        # Kill the app to force it to restart the mounted APK in case it's already running
+        $KILL
         """.trimIndent()
 }
