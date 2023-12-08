a99cf7f3743b793443e6991d0ee0d5bf4c778096
oSumAtrIX <johan.melkonyan1@web.de>
2023-12-08 16:56:55 +0100

    feat: Publicize constants

--- src/main/kotlin/app/revanced/library/installation/installer/Constants.kt
+++ src/main/kotlin/app/revanced/library/installation/installer/Constants.kt
@@ -1,32 +1,32 @@
 package app.revanced.library.adb
 
-internal object Constants {
-    internal const val PLACEHOLDER = "PLACEHOLDER"
+object Constants {
+    const val PLACEHOLDER = "PLACEHOLDER"
 
-    internal const val TMP_PATH = "/data/local/tmp/revanced.tmp"
-    internal const val INSTALLATION_PATH = "/data/adb/revanced/"
-    internal const val PATCHED_APK_PATH = "$INSTALLATION_PATH$PLACEHOLDER.apk"
-    internal const val MOUNT_SCRIPT_PATH = "/data/adb/service.d/mount_revanced_$PLACEHOLDER.sh"
+    const val TMP_PATH = "/data/local/tmp/revanced.tmp"
+    const val INSTALLATION_PATH = "/data/adb/revanced/"
+    const val PATCHED_APK_PATH = "$INSTALLATION_PATH$PLACEHOLDER.apk"
+    const val MOUNT_SCRIPT_PATH = "/data/adb/service.d/mount_revanced_$PLACEHOLDER.sh"
 
-    internal const val DELETE = "rm -rf $PLACEHOLDER"
-    internal const val CREATE_DIR = "mkdir -p"
-    internal const val RESTART = "am start -S $PLACEHOLDER"
-    internal const val KILL = "am force-stop $PLACEHOLDER"
-    internal const val GET_INSTALLED_PATH = "pm path $PLACEHOLDER"
+    const val DELETE = "rm -rf $PLACEHOLDER"
+    const val CREATE_DIR = "mkdir -p"
+    const val RESTART = "am start -S $PLACEHOLDER"
+    const val KILL = "am force-stop $PLACEHOLDER"
+    const val GET_INSTALLED_PATH = "pm path $PLACEHOLDER"
 
-    internal const val INSTALL_PATCHED_APK =
+    const val INSTALL_PATCHED_APK =
         "base_path=\"$PATCHED_APK_PATH\" && " +
             "mv $TMP_PATH ${'$'}base_path && " +
             "chmod 644 ${'$'}base_path && " +
             "chown system:system ${'$'}base_path && " +
             "chcon u:object_r:apk_data_file:s0  ${'$'}base_path"
 
-    internal const val UMOUNT =
+    const val UMOUNT =
         "grep $PLACEHOLDER /proc/mounts | while read -r line; do echo ${'$'}line | cut -d ' ' -f 2 | sed 's/apk.*/apk/' | xargs -r umount -l; done"
 
-    internal const val INSTALL_MOUNT_SCRIPT = "mv $TMP_PATH $MOUNT_SCRIPT_PATH && chmod +x $MOUNT_SCRIPT_PATH"
+    const val INSTALL_MOUNT_SCRIPT = "mv $TMP_PATH $MOUNT_SCRIPT_PATH && chmod +x $MOUNT_SCRIPT_PATH"
 
-    internal val MOUNT_SCRIPT =
+    val MOUNT_SCRIPT =
         """
         #!/system/bin/sh
         MAGISKTMP="$( magisk --path )" || MAGISKTMP=/sbin
