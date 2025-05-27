a820d52bff96c4fc06a61fc3a9326813dc8ee202
oSumAtrIX <johan.melkonyan1@web.de>
2025-05-27 15:18:05 +0200

    refactor

--- src/commonMain/kotlin/app/revanced/library/installation/installer/AdbInstaller.kt
+++ src/commonMain/kotlin/app/revanced/library/installation/installer/AdbInstaller.kt
@@ -1,13 +1,13 @@
 package app.revanced.library.installation.installer
 
 import app.revanced.library.installation.command.AdbShellCommandRunner
+import app.revanced.library.installation.command.ShellCommandRunner
+import app.revanced.library.installation.installer.Constants.GET_SDK_VERSION
 import app.revanced.library.installation.installer.Constants.INSTALLED_APK_PATH
-import app.revanced.library.installation.installer.Installer.Apk
 import se.vidstige.jadb.JadbException
 import se.vidstige.jadb.managers.Package
 import se.vidstige.jadb.managers.PackageManager
 import se.vidstige.jadb.managers.PackageManager.UPDATE_OWNERSHIP
-import java.nio.charset.StandardCharsets
 
 /**
  * [AdbInstaller] for installing and uninstalling [Apk] files using ADB.
@@ -19,26 +19,22 @@ import java.nio.charset.StandardCharsets
 class AdbInstaller(
     deviceSerial: String? = null,
 ) : Installer<AdbInstallerResult, Installation>() {
-    private val device = getDevice(deviceSerial, logger)
-    private val adbShellCommandRunner = AdbShellCommandRunner(device)
-    private val packageManager = PackageManager(device)
+    private val shellCommandRunner: ShellCommandRunner
+    private val packageManager: PackageManager
 
     init {
+        val device = getDevice(deviceSerial, logger)
+        shellCommandRunner = AdbShellCommandRunner(device)
+        packageManager = PackageManager(device)
+
         logger.fine("Connected to $deviceSerial")
     }
 
-    private fun getSdkVersion(): Int? = runCatching {
-        device.executeShell("getprop", "ro.build.version.sdk")
-            .bufferedReader(StandardCharsets.UTF_8)
-            .use { it.readLine()?.trim()?.toInt() }
-    }.getOrNull()
-
     override suspend fun install(apk: Apk): AdbInstallerResult {
-        getSdkVersion().let {
-            return runPackageManager {
-                if (it == null || it < 34) install(apk.file)
-                else installWithOptions(apk.file, arrayListOf(UPDATE_OWNERSHIP))
-            }
+        return runPackageManager {
+            val sdkVersion = shellCommandRunner(GET_SDK_VERSION).output.toInt()
+            if (sdkVersion < 34) install(apk.file)
+            else installWithOptions(apk.file, arrayListOf(UPDATE_OWNERSHIP))
         }
     }
 
@@ -50,7 +46,7 @@ class AdbInstaller(
 
     override suspend fun getInstallation(packageName: String): Installation? = packageManager.packages.find {
         it.toString() == packageName
-    }?.let { Installation(adbShellCommandRunner(INSTALLED_APK_PATH).output) }
+    }?.let { Installation(shellCommandRunner(INSTALLED_APK_PATH).output) }
 
     private fun runPackageManager(block: PackageManager.() -> Unit) = try {
         packageManager.run(block)
